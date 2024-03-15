def2b0e266de82a722ba8bcd210c49ec61121590
JendaSt <jendastraka@seznam.cz>
2024-03-15 11:17:00 +0100

    Selenium

---
+++ Selenium/Test.cs
@@ -0,0 +1,67 @@
+﻿namespace Selenium
+{
+    using System;    
+    using System.Threading;    
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using Selenium.PageObject;
+
+    public class Test
+    {
+        private IWebDriver driver;
+        private WikiPage _WikiPage;
+        private string page = "Filozofie";
+
+        [Test]
+        public void PhilosophyTest()
+        {            
+            OpenUrl("https://cs.wikipedia.org/wiki");
+            OpenArticle(page);
+            RedirectPage();
+            driver.Close();
+        }
+
+        public void OpenUrl(string url)
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = url;
+            Thread.Sleep(500);
+        }
+
+        public void OpenArticle(string article)
+        {
+            _WikiPage = new WikiPage(driver);
+            _WikiPage.searchField.SendKeys(article);
+            Thread.Sleep(500);
+            _WikiPage.searchBtn.Click();
+            Thread.Sleep(500);
+        }
+
+        public void RedirectPage()
+        {
+            int i = 0;
+            do
+            {
+                GetLink().Click();
+                Thread.Sleep(500);
+                Console.WriteLine(_WikiPage.firstHeading.FindElement(By.ClassName("mw-page-title-main")).Text);
+                i++;
+            }
+            while (_WikiPage.firstHeading.FindElement(By.ClassName("mw-page-title-main")).Text != page);
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Number of redirect = " + i);
+            Thread.Sleep(500);
+        }
+
+        public IWebElement GetLink()
+        {            
+            IWebElement link = _WikiPage.bodyContent.FindElement(By.CssSelector("[href ^= '/wiki/']"));
+            Console.WriteLine("-> " + link.Text);
+            Thread.Sleep(500);
+            return link;
+        }
+    }
+}
